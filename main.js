e37c646da97a01f3f88e6d549746e680f0f3e8c5
Mirko Bozzetto <mirkouch@gmail.com>
2024-03-04 14:27:06 +0100

    Ajouter un titre en gras et modifier l'opacité de l'élément apparaissant

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 import { gsap } from "gsap";
 
-gsap.fromTo("#appear", { opacity: 0 }, { opacity: 1, duration: 2.5 });
+gsap.fromTo("#appear", { opacity: 0.1 }, { opacity: 1, duration: 2.5 });
+
+gsap.fromTo(
+  "#bold",
+  { fontWeight: 100 },
+  { opacity: 1, duration: 2.5, fontWeight: 900 }
+);
